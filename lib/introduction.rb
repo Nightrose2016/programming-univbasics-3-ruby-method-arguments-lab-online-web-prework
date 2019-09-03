9aab8e314e1e372bc9f821400482a08845b8e096
Lialah Barrer <lrbarrer@gmail.com>
2019-09-03 00:24:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,11 +3,11 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language 
+def introduction_with_language (name , language)
   puts "Hi, my name is #{name} and I amlearning to program in #{language}"
 end
 
-def introduction_with_language_optional
+def introduction_with_language_optional (name , language ="ruby")
   puts "Hi, my name is #{name} and I amlearning to program in #{language}"
 end
 
